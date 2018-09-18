372944e4129d124abd76a4f5f673e27acbd107ad
Moritz Eichhorn <moritz.eichhorn@outlook.com>
2018-09-18 11:31:34 +0200

    Added Header

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,16 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: <1AHIF>
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			<Hello World>
+ * Author:			<Moritz Eichhorn>
+ * ----------------------------------------------------------
+ * Description:
+ * <Just prints out Hello Moritz Eichhorn
+ * >
+ * ----------------------------------------------------------
+ */
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
