05dc28baad47fdf39bdf3857515663d800445cae
MoritzEichhorn <moritz.eichhorn@outlook.com>
2018-09-23 17:04:08 +0200

    Installed Ubuntu at home and repeated the first exercise

---
+++ hello_world_wiederholung.c
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World_Wiederholung
+ * Author:			Moritz Eichhorn
+ * ----------------------------------------------------------
+ * Description:
+ * Repitition of Hello World
+ *
+ * ----------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("Hello Moritz Eichhorn, Wiederholung zu Hause\n");
+  return 0;
+}
